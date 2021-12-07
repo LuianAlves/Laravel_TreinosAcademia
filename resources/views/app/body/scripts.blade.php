b3829765e3b5cb54d377fa085a0abbbed2d124eb
Luian Alves <luian.alves@gmail.com>
2021-12-07 11:03:47 -0300

    Modificando recursos de Hospedagem pt.2

--- resources/views/app/body/scripts.blade.php
+++ resources/views/app/body/scripts.blade.php
@@ -2,19 +2,19 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
     {{-- Pesquisar Alunos com JQuery --}}
-    <script src="{{ asset('https://treinosacademia.herokuapp.com/backend/assets/js/main_search_alunos.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/main_search_alunos.js') }}"></script>
 
-    <script src="{{ asset('https://treinosacademia.herokuapp.com/backend/assets/vendor/bootstrap/js/bootstrap.bundle.js') }}"></script>
-    <script src="{{ asset('https://treinosacademia.herokuapp.com/backend/assets/vendor/php-email-form/validate.js') }}"></script>
-    <script src="{{ asset('https://treinosacademia.herokuapp.com/backend/assets/vendor/quill/quill.min.js') }}"></script>
-    <script src="{{ asset('https://treinosacademia.herokuapp.com/backend/assets/vendor/tinymce/tinymce.min.js') }}"></script>
-    <script src="{{ asset('https://treinosacademia.herokuapp.com/backend/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
-    <script src="{{ asset('https://treinosacademia.herokuapp.com/backend/assets/vendor/chart.js/chart.min.js') }}"></script>
-    <script src="{{ asset('https://treinosacademia.herokuapp.com/backend/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
-    <script src="{{ asset('https://treinosacademia.herokuapp.com/backend/assets/vendor/echarts/echarts.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/bootstrap/js/bootstrap.bundle.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/quill/quill.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/tinymce/tinymce.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/chart.js/chart.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/echarts/echarts.min.js') }}"></script>
 
     <!-- Template Main JS File -->
-    <script src="{{ asset('https://treinosacademia.herokuapp.com/backend/assets/js/main.js') }}"></script>   
+    <script src="{{ asset('backend/assets/js/main.js') }}"></script>   
 
     {{-- Esconder a Tela de Pesquisa quando clicar fora --}}
     <script>
